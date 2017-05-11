b06e12709d90b3c035cff4386ed2ad2ebf885195
lanmalkieri <cjstobie@gmail.com>
2017-05-11 21:42:23 +0000

    Updated

--- watch-watcher.py
+++ watch-watcher.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
+#---------------------------------------------------------------------------#
+# Written by: Christopher Stobie <cjstobie@gmail.com>                       #
+#---------------------------------------------------------------------------#
 import urllib2
 import lxml
-import texttable as tt
 import boto3
+import sys
+import argparse
+import texttable as tt
 from bs4 import BeautifulSoup
-from prettytable import PrettyTable
-
 
 #---------------------------------------------------------------------------#
-# Set whatever URL's you want to monitor here                               #
+# Global Vars                                                               #
+#---------------------------------------------------------------------------#
+# Supported websites: dutyfreeislandshop.com & longislandwatch.com          #
 #---------------------------------------------------------------------------#
 urls = ["http://www.dutyfreeislandshop.com/citizen-nh8350-83l-automatic-50m-elegant-mens-watch",
         "http://www.longislandwatch.com/Aeromatic_A1367_Military_Watch_p/a1367.htm"]
@@ -16,9 +21,22 @@ urls = ["http://www.dutyfreeislandshop.com/citizen-nh8350-83l-automatic-50m-eleg
 watches = {}
 char_count = []
 x=[[]]
-
+url_status = 'not_alerted'
+cache_file = '.watch_cache'
+char_count = []
 tab = tt.Texttable()
 
+#---------------------------------------------------------------------------#
+# Ensure our cache file exists                                              #
+#---------------------------------------------------------------------------#
+try:
+    f = open(cache_file)
+except IOError:
+    f = open(cache_file, 'w+')
+
+#---------------------------------------------------------------------------#
+# Get the html data for each URL, and store the stock status in a dict      #
+#---------------------------------------------------------------------------#
 for url in urls:
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, "lxml")
@@ -35,21 +53,52 @@ for url in urls:
 
     watches[product] = stock_status
 
-char_count = []
-
+#---------------------------------------------------------------------------#
+# Get the character count from each watch product and dynamically set the   #
+# column width for our table                                                #
+#---------------------------------------------------------------------------#
 for key, val in (watches.items()):
     char_count.append(len(key))
 
 longest_product = max(char_count)
 
+#---------------------------------------------------------------------------#
+# Set Product Status to alerted or not alerted based on file cache          #
+#---------------------------------------------------------------------------#
 for key,val in watches.items():
-    if "InStock" in val:
-        print("\nProduct %s is available!\n") %key
+    with open(cache_file) as search:
+        for line in search:
+            line = line.rstrip()
+            if key == line:
+                url_status = "already_alerted"
+
+#---------------------------------------------------------------------------#
+# If product is not alerted, check stock availility and act accordingly     #
+#---------------------------------------------------------------------------#
+    if url_status == 'not_alerted':
+        if "InStock" in val:
+            f = open(cache_file, 'a')
+            f.write(key + '\n')
+            f.close()
+            client = boto3.client('sns')
+            print("Publishing availability of %s to SNS") %key
+            response = client.publish(
+                TopicArn = "arn:aws:sns:us-west-2:438886243477:Watch-Watcher",
+                Message = "Watch %s is available!" %key
+            )
+
     x.append([key, val])
 
+#---------------------------------------------------------------------------#
+# Texttable settings                                                        #
+#---------------------------------------------------------------------------#
 tab.add_rows(x)
 tab.set_cols_align(['l', 'l'])
 tab.set_cols_width([longest_product, 12])
 tab.header(['Product', 'Availability'])
 
+
+#---------------------------------------------------------------------------#
+# Output all watches in urls list and their availability                    #
+#---------------------------------------------------------------------------#
 print(tab.draw())
